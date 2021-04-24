b788ade4063fec64ca0940879e2df98089c70de6
Fedir Katushonok <mtg.in.ua@gmail.com>
2021-04-24 21:15:16 +0300

    added TransformToSItests

--- SWaverTests/UnitTest1.cs
+++ SWaverTests/UnitTest1.cs
@@ -18,5 +18,13 @@ namespace SWaverTests
             var fading = FiderCalculation.FiderExtinction(10, 1);
             Assert.AreEqual(0.1, fading);
         }
+
+        [Test]
+        public void TransformToSystemInternational_Test()
+        {
+            MathObject mathObject = new MathObject(100, MetricPrefixes.k, UnitsOfMeasurement.Meter);
+            var rawValue = mathObject.TransformToSystemInternational();
+            Assert.AreEqual(rawValue, 100000);
+        }
     }
 }
