c1e5803ae89382c81fd986484cb493ad9bb5c48c
Fedir Katushonok <mtg.in.ua@gmail.com>
2021-05-10 12:27:44 +0300

    added GraphPoint

---
+++ SWaverLib/Utils/GraphPoint.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SWaverLib.Utils
+{
+    public struct GraphPoint
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public GraphPoint(double x, double y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+    }
+}
