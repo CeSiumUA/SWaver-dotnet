261bff8967b8c7179891fb0ae11de88fe6778e70
Fedir Katushonok <mtg.in.ua@gmail.com>
2021-05-10 11:31:35 +0300

    added React web view

---
+++ SWaverWeb/swaver/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import GraphChart from './chart';
+
+function App() {
+  return (
+    <GraphChart></GraphChart>
+  );
+}
+
+export default App;
